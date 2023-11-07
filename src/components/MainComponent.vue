e4a295da1a0742929a68f38ce196bb4ec7e57479
paulinely <paulinely@gmail.com>
2023-11-07 20:44:03 -0300

    inicio

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+   <p>To-Do APP</p> 
+   <hr>
+   <div class="tasks">
+    <span v-show="loading">loading...</span>
+    <div v-for="(task, idx) in tasks" v-bind:key="idx" class="task">
+      <TaskComponent v-bind:task="task" />
+    </div>
+   </div>
+  </div>
+</template>
+<script>
+import TaskComponent from './TaskComponent.vue';
+
+export default {
+  name: 'MainComponent',
+  components: {
+    TaskComponent,
+  },
+  data() {
+    return {
+      tasks: [],
+      loading: true
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.tasks = [
+        {
+          title: 'title1',
+          date: new Date(Date.now()),
+          msg: 'msg 111111111....',
+        },
+        {
+          title: 'title2',
+          date: new Date(Date.now()),
+          msg: 'msg 111111111....',
+        },
+        {
+          title: 'title3',
+          date: new Date(Date.now()),
+          msg: 'msg 111111111....',
+        },
+      ];
+      this.loading = false;
+    }, 1000);
+  } 
+}
+</script>
+<style scoped>
+  
+</style>
---
+++ src/components/TaskComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>{{task.title}}</h2>
+    <h5>{{task.date}}</h5>
+    <h5>{{task.msg}}</h5>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TaskComponent',
+  data() {
+    return {
+      
+    }
+  },
+  props: {
+    task: Object
+  }
+}
+</script>
+<style scoped>
+  
+</style>
